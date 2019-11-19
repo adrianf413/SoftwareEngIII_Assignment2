dc52742c1d81a2e97631f89789bd2d6ac7a1eea4
Adrian Forde <adrianforde4@gmail.com>
2019-11-19 17:43:38 +0000

    Adding unit test stub code for all 3 classes

---
+++ SoftwareEngIII_assignment2/src/test/java/CourseProgrammeTest.java
@@ -0,0 +1,56 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CourseProgrammeTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getModules() {
+    }
+
+    @Test
+    public void setModules() {
+    }
+
+    @Test
+    public void getEnrolled_students() {
+    }
+
+    @Test
+    public void setEnrolled_students() {
+    }
+
+    @Test
+    public void getCourse_start_date() {
+    }
+
+    @Test
+    public void setCourse_start_date() {
+    }
+
+    @Test
+    public void getCourse_end_date() {
+    }
+
+    @Test
+    public void setCourse_end_date() {
+    }
+}
---
+++ SoftwareEngIII_assignment2/src/test/java/StudentTest.java
@@ -0,0 +1,56 @@
+import org.junit.*;
+
+public class StudentTest {
+
+    @Test
+    public void getUsername() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getAge() {
+    }
+
+    @Test
+    public void setAge() {
+    }
+
+    @Test
+    public void getDate_of_birth() {
+    }
+
+    @Test
+    public void setDate_of_birth() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getCourse() {
+    }
+
+    @Test
+    public void setCourse() {
+    }
+
+    @Test
+    public void getModules() {
+    }
+
+    @Test
+    public void setModules() {
+    }
+}
