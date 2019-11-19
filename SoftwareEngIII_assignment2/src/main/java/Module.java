9e19b0b9f25a61065a2a90c35f555ed5ca36875f
Adrian Forde <adrianforde4@gmail.com>
2019-11-19 17:16:55 +0000

    Adding constructors and getter and setter methods

--- SoftwareEngIII_assignment2/src/main/java/Module.java
+++ SoftwareEngIII_assignment2/src/main/java/Module.java
@@ -5,5 +5,42 @@ public class Module {
     Student[] enrolled_students;
     CourseProgramme[] associated_courses;
 
+    public Module(String name, int id, Student[] enrolled_students, CourseProgramme[] associated_courses) {
+        this.name = name;
+        this.id = id;
+        this.enrolled_students = enrolled_students;
+        this.associated_courses = associated_courses;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Student[] getEnrolled_students() {
+        return enrolled_students;
+    }
+
+    public void setEnrolled_students(Student[] enrolled_students) {
+        this.enrolled_students = enrolled_students;
+    }
+
+    public CourseProgramme[] getAssociated_courses() {
+        return associated_courses;
+    }
+
+    public void setAssociated_courses(CourseProgramme[] associated_courses) {
+        this.associated_courses = associated_courses;
+    }
 }
